9cace1792a5a451b474d178572b39627f04663bb
Tran Van Truong <contact@tranvantruong.com>
2018-12-27 21:01:08 +0700

    Add 1 float method into area_triangle.c

--- W3/area_triangle.c
+++ W3/area_triangle.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 typedef struct point_st
 {
-int x;
-int y;
+    int x;
+    int y;
 } point_t;
 
 typedef struct triangle
 {
-point_t A;
-point_t B;
-point_t C;
+    point_t A;
+    point_t B;
+    point_t C;
 } triangle_t;
 
 float calculation_area(triangle_t ABC);
 
 void main(void)
 {
-    triangle_t ABC = {{1,2}, {2,1}, {9,5}};
+    triangle_t ABC = {{0,0}, {3,0}, {0,1}};
     float result;
     result = calculation_area (ABC);
     printf("The area of the triangle ABC is: %.2f\n", result);
@@ -28,7 +29,8 @@ void main(void)
 float calculation_area(triangle_t ABC)
 {
     float area;
-    area = ABC.A.x*(ABC.B.y-ABC.C.y)+ABC.B.x*(ABC.C.y-ABC.A.y)+ABC.C.x*(ABC.A.y-ABC.B.y);
-    area = area/2;
+    area = fabs((ABC.A.x*(ABC.B.y-ABC.C.y)+ABC.B.x*(ABC.C.y-ABC.A.y)+ABC.C.x*(ABC.A.y-ABC.B.y))/2.0);
+    // area = ABC.A.x*(ABC.B.y-ABC.C.y)+ABC.B.x*(ABC.C.y-ABC.A.y)+ABC.C.x*(ABC.A.y-ABC.B.y);
+    // area = fabs(area/2);
     return area;
 }

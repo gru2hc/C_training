63c5808a2fc9c23c2f1cb68ab3fad8616a097537
Tran Van Truong <contact@tranvantruong.com>
2018-12-27 21:19:20 +0700

    Replace datatype input of calculate_area

--- W3/area_triangle.c
+++ W3/area_triangle.c
@@ -15,21 +15,21 @@ typedef struct triangle
     point_t C;
 } triangle_t;
 
-float calculate_area(triangle_t ABC);
+float calculate_area(triangle_t *p_ABC);
 
 void main(void)
 {
     triangle_t ABC = {{0,0}, {3,0}, {0,1}};
     float result;
-    result = calculate_area (ABC);
+    result = calculate_area (&ABC);
     printf("The area of the triangle ABC is: %.2f\n", result);
     return;
 }
 
-float calculate_area(triangle_t ABC)
+float calculate_area(triangle_t *p_ABC)
 {
     float area;
-    area = fabs((ABC.A.x*(ABC.B.y-ABC.C.y)+ABC.B.x*(ABC.C.y-ABC.A.y)+ABC.C.x*(ABC.A.y-ABC.B.y))/2.0);
+    area = fabs(((*p_ABC).A.x*((*p_ABC).B.y-(*p_ABC).C.y)+(*p_ABC).B.x*((*p_ABC).C.y-(*p_ABC).A.y)+(*p_ABC).C.x*((*p_ABC).A.y-(*p_ABC).B.y))/2.0);
     // area = ABC.A.x*(ABC.B.y-ABC.C.y)+ABC.B.x*(ABC.C.y-ABC.A.y)+ABC.C.x*(ABC.A.y-ABC.B.y);
     // area = fabs(area/2);
     return area;

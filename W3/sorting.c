b24aa01c67096b7736fadee28f3891bbf076ca91
Tran Van Truong <contact@tranvantruong.com>
2018-12-27 22:26:46 +0700

    Add decending method

--- W3/sorting.c
+++ W3/sorting.c
@@ -4,17 +4,24 @@
 void arrange_array(int array[], int n);
 void comparate_2_nums(int *a, int *b);
 void print_array(int array[], int n);
+void decending(int array[], int n);
+void swap(int *a, int *b);
 
+void t_arrange_array(int array[], int n);
+void t_comparate_2_nums(int *a, int *b);
 void main(void)
 {
-    int a[] = {4,2,8,9,7,90,8,67,46,57,0};
+    int a[] = {4,8,9,7,90,8,67,46,57,0};
     int n;
     n = sizeof(a)/sizeof(int);
     print_array(&(a[0]),n);
     printf("\n");
-    arrange_array(&(a[0]),n);
+    t_arrange_array(&(a[0]),n);
     printf("\nResult after sorting as below.\n");
     print_array(&(a[0]),n);
+    //printf("Result after rearrangement.\n");
+    //decending(&(a[0]),n);
+    //print_array(&(a[0]),n);
     return;
 }
 
@@ -33,6 +40,32 @@ void arrange_array(int array[], int n)
     return;
 }
 
+void t_arrange_array(int array[], int n)
+{
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            t_comparate_2_nums(&(array[i]),&(array[j]));
+            //printf("%d\n", array[i]);
+            // print_array(&(array[0]),n);
+        }
+    }
+    return;
+}
+
+void t_comparate_2_nums(int *a, int *b)
+{
+    int temp;
+    if (*a < *b)
+    {
+        temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+    return;
+}
 
 void comparate_2_nums(int *a, int *b)
 {
@@ -54,5 +87,26 @@ void print_array(int array[], int n)
     {
         printf("%d ",array[i]);
     }
+    return;
+}
+
+void decending(int array[], int n)
+{
+    int i;
+    // n =n/2;
+    for(i=0;i<(n/2);i++)
+    {
+        swap(&(array[i]),&(array[n-i-1]));
+        printf("%d \n", array[i]);
+    }
+    return;
+}
+
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
     return;
 }

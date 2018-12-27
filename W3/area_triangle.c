2beec39041d8b651f7386041a1a25781c4a52a3d
Tran Van Truong <contact@tranvantruong.com>
2018-12-27 21:02:52 +0700

    Fix grammar in area_triangle.c

--- W3/area_triangle.c
+++ W3/area_triangle.c
@@ -15,18 +15,18 @@ typedef struct triangle
     point_t C;
 } triangle_t;
 
-float calculation_area(triangle_t ABC);
+float calculate_area(triangle_t ABC);
 
 void main(void)
 {
     triangle_t ABC = {{0,0}, {3,0}, {0,1}};
     float result;
-    result = calculation_area (ABC);
+    result = calculate_area (ABC);
     printf("The area of the triangle ABC is: %.2f\n", result);
     return;
 }
 
-float calculation_area(triangle_t ABC)
+float calculate_area(triangle_t ABC)
 {
     float area;
     area = fabs((ABC.A.x*(ABC.B.y-ABC.C.y)+ABC.B.x*(ABC.C.y-ABC.A.y)+ABC.C.x*(ABC.A.y-ABC.B.y))/2.0);

9ee510501c5224323396150b54218c6972f6faf2
Tran Van Truong <contact@tranvantruong.com>
2019-01-11 22:31:15 +0700

    Add linked_list + display odd number

--- W5/display.c
+++ W5/display.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
 
-void main(void)
+int main(void)
 {
     int n,i,j,h;
-    printf("Please enter the number:\n");
+    printf("Please enter the odd number:\n");
     scanf("%d", &n);
+    if(n%2==0)
+    {
+        printf("Invalid number. Please enter odd number!\n");
+        return -1;
+    }
     h = (n-1)/2;
     printf("This is output:\n");
     for(i=0;i<(n+1)/2;i++)
     {
         for(j=0;j<n;j++)
         {
-            // if(i<(h+1))
-            // {
-                if((j<(h-i)) || (j>(h+i)))
-                {
-                    printf(" ");
-                }
-                else
-                {
-                    printf("*");
-                }
-            // }
-            // else if(i>(h-1))
-            // {
-                // if((j<(h-(i/2))) || (j>(h+(i/2))))
-                // {
-                    // printf(" ");
-                // }
-                // else
-                // {
-                    // printf("*");
-                // }
-            // }
+            if((j<(h-i)) || (j>(h+i)))
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("*");
+            }
         }
         printf("\n");
     }
-    return;
+    for(i=(n+1)/2;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if((j<(i-h)) || (j>(n-1)-(i-h)))
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
 }
--- W5/main.c
+++ W5/main.c
@@ -3,25 +3,83 @@
 typedef struct node
 {
     int data;
-    struct node* p;
+    struct node* next;
 } node_t;
 
-void display_node(void);
+typedef node_t* head_t;
 
-node_t* head = NULL;
-node_t* temp;
+void add_node(head_t *head, node_t* new_node);
+int delete_node(head_t *head);
+void display(head_t head);
+
+head_t g_head = NULL;
+node_t* con_node;
 
 void main(void)
 {
-    node_t node1= {4, NULL};
-    head = &node1;
-    node_t node2= {5, NULL};
-    temp.p = &node2;
-    display_node();
+    int temp;
+    node_t nodeA = {4,NULL};
+    node_t nodeB = {5,NULL};
+    node_t nodeC = {6,NULL};
+    node_t nodeD = {7,NULL};
+
+    add_node(&g_head, &nodeA);
+    add_node(&g_head, &nodeB);
+    add_node(&g_head, &nodeC);
+    add_node(&g_head, &nodeD);
+
+    display(g_head);
+
+    temp = delete_node(&g_head);
+    display(g_head);
     return;
 }
 
-void display_node(void)
+void add_node(head_t *head, node_t* new_node)
 {
-    printf("Members in node: %d, %x\n", head->data, head->p);
+    if(*head == NULL)
+    {
+        *head = new_node;
+    }
+    else
+    {
+        con_node=*head;
+
+        while (1)
+        {
+            if(con_node->next!=NULL)
+            {
+                con_node = con_node->next;
+            }
+            else
+            {
+                break;
+            }
+        }
+        con_node->next = new_node;
+    }
+}
+
+int delete_node(head_t *head)
+{
+    if((*head) != NULL)
+    {
+        (*head) = (*head)->next;
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+void display(head_t head)
+{
+    printf("Element of each node:\n");
+    node_t* temp=head;
+    while (temp !=NULL)
+    {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
 }
